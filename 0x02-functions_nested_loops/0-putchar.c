a15d53b439e2ce6ba70d8eaccc345f3aa88ffeea
Mattakinremi <mattakinremi@gmail.com>
2023-09-12 16:22:25 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - This describes the function to write _putchar.
+ *
+ * Return: returns the value 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
